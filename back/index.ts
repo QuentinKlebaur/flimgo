b8139493853050b1ece1a6a1ec48133d92eecdd9
QuentinKlebaur <quentin.klebaur@epitech.eu>
2024-11-26 22:18:34 +0100

    feat: add PGAdmin + connect back to DB

--- back/index.ts
+++ back/index.ts
@@ -7,8 +7,27 @@ import TileRoute  from './routes/TileRoute';
 import express from 'express';
 import swaggerUi from "swagger-ui-express";
 const swaggerjsonFilePath = require("./swagger.json");
+const { Client } = require('pg');
+require('dotenv').config()
 
 const app = express();
+
+const client = new Client({
+	user: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+	host: process.env.POSTGRES_HOST,
+	port: process.env.POSTGRES_PORT,
+	database: process.env.POSTGRES_DB_NAME,
+});
+
+client
+	.connect()
+	.then(() => {
+		console.log('Connected to PostgreSQL database');
+	})
+	.catch((err: any) => {
+		console.error('Error connecting to PostgreSQL database', err);
+	});
 
 app.use('/authentication', AuthenticationRoute);
 app.use('/bingos', BingoRoute);
